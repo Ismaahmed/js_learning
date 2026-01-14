a654e17c51fbded1526f353c469a6caa222d7108
Ismaahmed <muhiza53@gmail.com>
2026-01-14 21:14:35 +0300

    commit

---
+++ app.js
@@ -0,0 +1,28 @@
+let name = "Ismahan";
+const age = 20;
+var city = "Mogadishu";
+
+// data type
+let text = "Hello";      // string
+let number = 10;        // number
+let isOnline = true;    // boolean
+let empty = null;       // null
+let x;                  // undefined
+
+
+// operator 
+let a = 5 + 3;   // 8
+let b = 10 - 2;  // 8
+let c = 4 * 2;   // 8
+let d = 16 / 2;  // 8
+
+
+//function
+
+function sayHello() {
+  console.log("Hello World");
+}
+
+sayHello();
+
+
